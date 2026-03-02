2e6f5984c6fd4cadf70328724480ab16552df818
Lin Jen-Shin <godfat@godfat.org>
2026-03-03 00:21:55 +0800

    See if we're able to log the inputs for segfault

--- lib/battle-cats-rolls/seek_seed.rb
+++ lib/battle-cats-rolls/seek_seed.rb
@@ -97,7 +97,14 @@ module BattleCatsRolls
           err: %i[child out]], 'r+') do |io|
           io.puts source.sub(/\A\S+ /, '')
           io.close_write
-          io.read.scan(/\d+/).map(&:to_i)
+          out = io.read
+          result = out.scan(/\d+/).map(&:to_i)
+
+          if result.empty? && !out.start_with?('No seeds found.')
+            logger.warn("Seeking seed failed with #{source}")
+          end
+
+          result
         end
       else
         []
