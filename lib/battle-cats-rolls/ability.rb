f537b61605ffd319828bcf1e9f19b00008701a6c
Lin Jen-Shin <godfat@godfat.org>
2026-06-29 16:39:52 +0800

    Add "when hit" for dodging. This clarifies when it'll happen

--- lib/battle-cats-rolls/ability.rb
+++ lib/battle-cats-rolls/ability.rb
@@ -305,7 +305,7 @@ module BattleCatsRolls
 
       def display values=nil, &block
         sprintf(
-          '%{chance} to become invulnerable for %{duration}',
+          '%{chance} to become invulnerable when hit for %{duration}',
           values || display_values(&block))
       end
 
