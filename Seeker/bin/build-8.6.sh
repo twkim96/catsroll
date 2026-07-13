a4c192041b9db0cbd436713174bf4f48d518c95f
Lin Jen-Shin <godfat@godfat.org>
2026-07-13 23:33:58 +0800

    Remove all legacy seekers
    They're not used for quite some time. There's no plan to maintain them

--- Seeker/bin/build-8.6.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-root=$(realpath $(dirname $0)/..)
-clang++ -std=c++11 -O2 $root/Seeker-8.6.cpp -o $root/Seeker-8.6
--- Seeker/bin/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-root=$(realpath $(dirname $0)/..)
-ghc -odir $root/build -hidir $root/build -O2 -threaded -with-rtsopts=-N -i$root $root/Main.hs -o $root/Seeker
