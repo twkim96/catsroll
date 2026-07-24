86716808bc26e45e34ff7a891cbbf5d31511df2d
Lin Jen-Shin <godfat@godfat.org>
2026-07-25 02:41:52 +0800

    Scan over /cats/#{id}?lang=jp to ensure all 200

--- test/test_web.rb
+++ test/test_web.rb
@@ -1,13 +1,17 @@
 
 require 'pork/auto'
 require 'battle-cats-rolls/web'
+require 'battle-cats-rolls/route'
 
 describe BattleCatsRolls::Web do
   web = BattleCatsRolls::Web.new
   web.call('PATH_INFO' => '/warmup')
 
-  define_method(:expect_status_200) do |path|
-    status, _headers, _body = web.call('PATH_INFO' => path)
+  BattleCatsRolls::Route.reload_balls
+
+  define_method(:expect_status_200) do |path, query=''|
+    status, _headers, _body = web.call(
+      'PATH_INFO' => path, 'QUERY_STRING' => query)
 
     expect(status).eq 200
   end
@@ -24,5 +28,11 @@ describe BattleCatsRolls::Web do
 
   would 'respond 200 for a non-existing cat' do
     expect_status_200('/cats/9999')
+  end
+
+  BattleCatsRolls::Route.ball_jp.cats.each_key do |id|
+    would "respond 200 for /cats/#{id}?lang=jp" do
+      expect_status_200("/cats/#{id}", 'lang=jp')
+    end
   end
 end
