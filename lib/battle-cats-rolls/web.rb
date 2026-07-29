0292ef77a79404a07bc1718db3b519539b5b34c7
Lin Jen-Shin <godfat@godfat.org>
2026-07-30 02:23:06 +0800

    Properly detect if there are 10 inputs for seeking
    The feature is that if there are 10 inputs, we show the 3rd link to
    account for the guaranteed roll because we don't know if the user rolled
    on a guaranteed event or not. If not they use the 2nd link, and if they
    did they use the 3rd link.
    
    The problem is that the result page doesn't have access to the original
    inputs. Previously, it was guessed by rolling the starting seed for 10
    times to see if that would match the current seed. If it matches, it means
    the user had input 10 rolls.
    
    This works when there are no duped rare cats in the rolls.
    
    This breaks when there are, because for each duped rare cats, it'll roll
    one additional time. It would be painful to properly simulate this,
    and this workaround was done before duped rare cats is a thing.
    
    Now we try to append a flag to the results to indicate this, where we
    have the original source of inputs, and then we check this flag instead.
    
    This is good because this is accurate, looking at the original source.
    This is bad because now the code is not quite readable due to magic
    numbers 4 and 30. It's 4 because we insert into the 5th spot in the
    result array, and it's 30 because each roll has 2 and the metadata has
    10 to begin with, so 10 + 2*10 == 30, meaning that there are 10 inputs.
    
    This also makes the protocol a bit rigid by assuming the index of the
    array. However I want this array to be flat, so this is possibly easier.
    
    We also avoid using a Ruby struct inside the cache, due to the concern
    of serializing into memcached.

--- lib/battle-cats-rolls/web.rb
+++ lib/battle-cats-rolls/web.rb
@@ -80,18 +80,9 @@ module BattleCatsRolls
         end
       end
 
-      def made10rolls? result
-        return unless result.starting_seed
-
-        gacha = Gacha.new(route.gacha.pool, result.starting_seed)
-        gacha.send(:advance_seed!) # Account offset
-        gacha.send(:advance_seed!) if result.run_type > 0 # Account duped rare
-        9.times{ gacha.roll! } # Only 9 rolls left
-
-        if gacha.seed == result.current_seed
-          gacha.send(:advance_seed!) # Account for guaranteed roll
-          gacha.seed
-        end
+      def set_offset_seed result
+        gacha = Gacha.new(route.gacha.pool, result.current_seed)
+        result.offset_seed = gacha.seed
       end
 
       def cache
@@ -275,7 +266,7 @@ module BattleCatsRolls
         key = m[:key]
         seek = SeekSeed.queue[key]
         result = SeekSeed.resolve(cache[key]) if /./.match?(key)
-        result.offset_seed = made10rolls?(result) if result
+        set_offset_seed(result) if result&.made10rolls
 
         seek.yield if seek&.ended?
 
